b822a232cad5a53a5b8554431e05a584439c3a2a
l-Spook-l <uaspookua@gmail.com>
2023-09-21 18:57:01 +0300

    change all models

--- src/auth/models.py
+++ src/auth/models.py
@@ -15,9 +15,9 @@ from src.database import Base
 class Role(Base):
     __tablename__ = "role_table"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
     name: Mapped[str] = mapped_column(String(length=99), nullable=False)
-    permissions: Mapped[JSON] = mapped_column(JSON)
+    # permissions: Mapped[JSON] = mapped_column(JSON, nullable=True)
 
     # тут список потому что много пользователей будет
     user: Mapped[list["User"]] = relationship(back_populates="role")
@@ -26,14 +26,14 @@ class Role(Base):
 class User(SQLAlchemyBaseUserTable[int], Base):
     __tablename__ = "user_table"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, unique=True)
-    email: Mapped[str] = mapped_column(String, nullable=False)
-    first_name: Mapped[str] = mapped_column(String, nullable=False)
-    second_name: Mapped[str] = mapped_column(String, nullable=False)
-    phone: Mapped[int] = mapped_column(Integer, nullable=False)
-    username: Mapped[str] = mapped_column(String, nullable=False)
-    registered_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default=datetime.utcnow())
-    role_id: Mapped[int] = mapped_column(Integer, ForeignKey("role_table.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    first_name: Mapped[str] = mapped_column(String(length=99), nullable=False)
+    second_name: Mapped[str] = mapped_column(String(99), nullable=False)
+    phone: Mapped[int | None] = mapped_column(nullable=False)  # необязательное поле [int | None]
+    username: Mapped[str] = mapped_column(String(99), nullable=False, unique=True)
+    registered_at: Mapped[datetime] = mapped_column(default=datetime.utcnow())
+    role_id: Mapped[int] = mapped_column(ForeignKey("role_table.id"))
 
     # из класса просто для простоты вынесем сюда
     hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
@@ -41,35 +41,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
+    # back_populates="user - на что ссылаемся, cascade="all" - при удалении польз. удалить все
     role: Mapped["Role"] = relationship(back_populates="user")
-    workout: Mapped[list["Workout"]] = relationship(back_populates="user")
-    set: Mapped[list["Set"]] = relationship(back_populates="user")
-
-# class Role(Base):
-#     __tablename__ = "role"
-#
-#     id = Column(Integer, primary_key=True, index=True, unique=True)
-#     name = Column(String, nullable=False)
-#     permissions = Column(JSON)
-#
-#     user = relationship("User", back_populates="role")
-
-# class User(SQLAlchemyBaseUserTable[int], Base):
-#     __tablename__ = "user"
-#
-#     id = Column(Integer, primary_key=True, index=True, unique=True)
-#     email = Column(String, nullable=False)
-#     first_name = Column(String, nullable=False)
-#     second_name = Column(String, nullable=False)
-#     phone = Column(Integer, nullable=False)
-#     username = Column(String, nullable=False)
-#     registered_at = Column(TIMESTAMP, default=datetime.utcnow())
-#     role_id = Column(Integer, ForeignKey("role.id"))
-#
-#     # из класса просто для простоты вынесем сюда
-#     hashed_password = Column(String(length=1024), nullable=False)
-#     is_active = Column(Boolean, default=True, nullable=False)
-#     is_superuser = Column(Boolean, default=False, nullable=False)
-#     is_verified = Column(Boolean, default=False, nullable=False)
-#
-#     role = relationship("Role", back_populates="users")
+    workout: Mapped[list["Workout"]] = relationship(back_populates="user", cascade="all")
+    set: Mapped[list["Set"]] = relationship(back_populates="user", cascade="all")
--- src/workouts/models.py
+++ src/workouts/models.py
@@ -13,29 +13,28 @@ from src.database import Base
 class Workout(Base):
     __tablename__ = "workout_table"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
     name: Mapped[str] = mapped_column(String(length=256))
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user_table.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"))
     description: Mapped[str] = mapped_column(String(length=1000))
-    created_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default=datetime.utcnow())
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow())
     difficulty: Mapped[str] = mapped_column(String(length=50))
-    timer: Mapped[DateTime] = mapped_column(DateTime)
 
     user: Mapped["User"] = relationship(back_populates="workout")
-    exercises: Mapped[list["Exercise"]] = relationship(back_populates="workout")
+    exercise: Mapped[list["Exercise"]] = relationship(back_populates="workout")
 
 
 class Exercise(Base):
     __tablename__ = "exercise_table"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, unique=True)
-    workout_id: Mapped[int] = mapped_column(Integer, ForeignKey("workout_table.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
+    workout_id: Mapped[int] = mapped_column(ForeignKey("workout_table.id"))
     description: Mapped[str] = mapped_column(String(length=500))
-    number_of_sets: Mapped[int] = mapped_column(Integer)
-    maximum_repetitions: Mapped[int] = mapped_column(Integer)
-    rest_time: Mapped[DateTime] = mapped_column(DateTime)
-    weight: Mapped[int] = mapped_column(Integer)
-    timer: Mapped[DateTime] = mapped_column(DateTime)
+    number_of_sets: Mapped[int]
+    maximum_repetitions: Mapped[int]
+    rest_time: Mapped[datetime]
+    weight: Mapped[int]
+    timer: Mapped[datetime]
 
     workout: Mapped["Workout"] = relationship(back_populates="exercise")
     set: Mapped[list["Set"]] = relationship(back_populates="exercise")
@@ -44,10 +43,10 @@ class Exercise(Base):
 class Set(Base):
     __tablename__ = "set_table"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, unique=True)
-    exercises_id: Mapped[int] = mapped_column(Integer, ForeignKey("exercise_table.id"))
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user_table.id"))
-    count: Mapped[int] = mapped_column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
+    exercise_id: Mapped[int] = mapped_column(ForeignKey("exercise_table.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"))
+    count: Mapped[int]
 
-    exercises: Mapped["Exercise"] = relationship(back_populates="set")
+    exercise: Mapped["Exercise"] = relationship(back_populates="set")
     user: Mapped["User"] = relationship(back_populates="set")
