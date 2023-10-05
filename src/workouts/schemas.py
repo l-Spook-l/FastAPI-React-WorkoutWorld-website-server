ae1acfd6bb9e76fb5971ed371aa6034be63b7abd
l-Spook-l <uaspookua@gmail.com>
2023-10-05 16:04:24 +0300

    made slight changes to the fields in some schemas.

--- src/workouts/schemas.py
+++ src/workouts/schemas.py
@@ -9,6 +9,7 @@ class WorkoutCreate(BaseModel):
     user_id: int
     description: str
     difficulty: str
+    total_time: str
 
 
 class ExerciseCreate(BaseModel):
@@ -17,14 +18,15 @@ class ExerciseCreate(BaseModel):
     number_of_sets: int
     maximum_repetitions: int
     rest_time: str
-    weight: int = Field(ge=0)  # число должно быть >= 0
-    timer: str
+    video: str
+    photo: str
 
 
 class SetCreate(BaseModel):
     exercise_id: int
     user_id: int
     count: int
+    weight: int = Field(ge=0)  # число должно быть >= 0
 
 
 class SetUpdate(BaseModel):
