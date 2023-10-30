b3e68b409e0b58615d5a8c93e9da872999d7a83c
l-Spook-l <uaspookua@gmail.com>
2023-10-30 15:44:59 +0200

    You can retrieve a token if the user forgets their password

---
+++ src/auth/manager.py
@@ -0,0 +1,67 @@
+# КОД ИЗ ДОКУМЕНТАЦИИ только поменял - IntegerIDMixin
+from typing import Optional
+
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, exceptions, models, schemas
+from fastapi_users import IntegerIDMixin  # для последовательного id для пользователя
+
+from .models import User
+from .utils import get_user_db
+
+# ключ
+SECRET = "SECRET"
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    # разные фун-ии
+    # после регистрации делаем что-то
+    async def on_after_register(self, user: User, request: Optional[Request] = None):
+        print(f"User {user.id} has registered.")
+
+    # Нужные фун-и, но не сейчас
+    # Забыл пароль
+    async def on_after_forgot_password(
+            self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+    # после запроса верификации
+    # async def on_after_request_verify(
+    #         self, user: User, token: str, request: Optional[Request] = None
+    # ):
+    #     print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+    # фун-я из класса BaseUserManager, мы ее немного меняем
+    # фун-я создает пользователя
+    async def create(
+            self,
+            user_create: schemas.UC,
+            safe: bool = False,
+            request: Optional[Request] = None,
+    ) -> models.UP:
+        await self.validate_password(user_create.password, user_create)
+
+        existing_user = await self.user_db.get_by_email(user_create.email)
+        if existing_user is not None:
+            raise exceptions.UserAlreadyExists()
+
+        user_dict = (
+            user_create.create_update_dict()
+            if safe
+            else user_create.create_update_dict_superuser()
+        )
+        password = user_dict.pop("password")
+        user_dict["hashed_password"] = self.password_helper.hash(password)
+        user_dict["role_id"] = 2  # при регистрации выставляем пользователю 2ю роль
+
+        created_user = await self.user_db.create(user_dict)
+
+        await self.on_after_register(created_user, request)
+
+        return created_user
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
