6cf6e3002ac3fd320e30536734722abb33ca4c6c
l-Spook-l <uaspookua@gmail.com>
2023-12-04 17:35:51 +0200

    Sending messages to the admin has been implemented along with sending limitations.

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Depends
-
 from .base_config import auth_backend, fastapi_users
 from .models import User
-from .schemas import UserRead, UserCreate, UserUpdate, PasswordResetRequest, PasswordReset
+from .schemas import UserRead, UserCreate, UserUpdate, PasswordResetRequest, SendMessageAdmin
 from fastapi.exceptions import HTTPException
 from .manager import UserManager
-from sqlalchemy import select, insert, update, func, delete
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
-from .utils import get_user_db, send_token_by_email
+from .utils import get_user_db, send_token_by_email, send_message_to_admin
+import time
 
+last_sent_time = 0
 router = APIRouter()
 
 # авторизация
@@ -32,31 +33,30 @@ router.include_router(
 
 @router.post("/request-password-reset")
 async def request_password_reset(request: PasswordResetRequest, session: AsyncSession = Depends(get_async_session)):
-    # Находим пользователя в базе данных по email
     query_user = await session.execute(select(User).filter(User.email == request.email))
     user = query_user.one()[0]
-    print('user1', user)
 
     if not user:
         raise HTTPException(status_code=404, detail="Workout not found")
 
     user_manager = UserManager(get_user_db)
     reset_token = await user_manager.forgot_password(user)
-    # Отправьте reset_token пользователю (например, по почте)
-    print('token', reset_token)
     await send_token_by_email(user.email, reset_token)
+
     return {"message": "If the email exists, a password reset link has been sent.", 'token': reset_token}
 
 
-# @router.post("/reset-password")
-# async def reset_password(reset_data: PasswordReset):
-#     print('token', reset_data.token)
-#     print('new_password', reset_data.new_password)
-#     # Измените пароль пользователя в базе данных
-#     user_manager = UserManager(get_user_db)
-#     user = await user_manager.reset_password(token=reset_data.token, password=reset_data.new_password)
-#
-#     if user:
-#         return {"message": "Password has been reset."}
-#     else:
-#         raise HTTPException(status_code=400, detail="Invalid or expired token.")
+@router.post('/send-message-admin')
+async def send_message_admin(message: SendMessageAdmin):
+    global last_sent_time
+
+    current_time = time.time()
+    time_difference = current_time - last_sent_time
+
+    if time_difference >= 180:
+        await send_message_to_admin(message.name, message.email, message.message)
+        last_sent_time = current_time
+        return {'status': 'success'}
+    else:
+        raise HTTPException(status_code=429, detail='You have already sent a message. '
+                                                    'Please wait for 3 minutes before sending another message')
